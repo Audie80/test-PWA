49e3592bbb54f1c7878636e7031a83710c436042
Audie80 <audie.menut@gmail.com>
2021-05-21 19:01:25 +0200

    creation of PWA

---
+++ js/app.js
@@ -0,0 +1,68 @@
+const container = document.querySelector(".container");
+const coffees = [
+  { name: "Perspiciatis", image: "images/coffee1.jpg" },
+  { name: "Voluptatem", image: "images/coffee2.jpg" },
+  { name: "Explicabo", image: "images/coffee3.jpg" },
+  { name: "Rchitecto", image: "images/coffee4.jpg" },
+  { name: " Beatae", image: "images/coffee5.jpg" },
+  { name: " Vitae", image: "images/coffee6.jpg" },
+  { name: "Inventore", image: "images/coffee7.jpg" },
+  { name: "Veritatis", image: "images/coffee8.jpg" },
+  { name: "Accusantium", image: "images/coffee9.jpg" },
+];
+const showCoffees = () => {
+  let output = "";
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  );
+  container.innerHTML = output;
+};
+
+document.addEventListener("DOMContentLoaded", showCoffees);
+
+// Enregistrement du serviceWorker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
+
+// Add to desktop
+// Doc here : https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Add_to_home_screen
+let deferredPrompt;
+const addBtn = document.querySelector(".add-button");
+addBtn.style.display = "none";
+window.addEventListener("beforeinstallprompt", (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = "block";
+
+  addBtn.addEventListener("click", (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = "none";
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      deferredPrompt = null;
+    });
+  });
+});
---
+++ serviceWorker.js
@@ -0,0 +1,38 @@
+const staticDevCoffee = "dev-coffee-site-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/coffee1.jpg",
+  "/images/coffee2.jpg",
+  "/images/coffee3.jpg",
+  "/images/coffee4.jpg",
+  "/images/coffee5.jpg",
+  "/images/coffee6.jpg",
+  "/images/coffee7.jpg",
+  "/images/coffee8.jpg",
+  "/images/coffee9.jpg",
+];
+
+// self is the service worker itself (the file)
+// Mise en cache des assets à l'install du serviceWorker (once)
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// Doc sur les différents fetch :
+// https://jakearchibald.com/2014/offline-cookbook/
+
+// Fetch des assets du cache offline-first
+self.addEventListener("fetch", (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
